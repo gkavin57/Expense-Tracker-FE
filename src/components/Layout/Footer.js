24205ed636aa8f0d505d9fcd257b59d46406aa17
gkavin57 <gkavin77@gmail.com>
2023-01-26 14:29:29 +0530

    latout and routing setup

---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-dark text-light p-4">
+      <h1 className="text-center">All rights reserved &copy; kavin</h1>
+    </div>
+  );
+};
+
+export default Footer;
